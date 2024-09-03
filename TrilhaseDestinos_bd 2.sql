e8f485bd19f739f860f7590073e5577d30b1ccbc
DaniloPires07 <danilodanilopires1@gmail.com>
2024-09-03 17:34:35 -0300

    Update TrilhaseDestinos_bd 2.sql

--- TrilhaseDestinos_bd 2.sql
+++ TrilhaseDestinos_bd 2.sql
@@ -6,7 +6,7 @@ preco DECIMAL (10, 2),
 id_destino INT,
 FOREIGN KEY (id_destino) REFERENCES Destinos(id_destino)
 );
-@ CREATE TABLE Reservas (
+ CREATE TABLE Reservas (
 id_reserva INT PRIMARY KEY AUTO_INCREMENT,
 id_cliente INT,
 id_pacote INT,
@@ -15,7 +15,7 @@ status VARCHAR(20),
 FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente),
 FOREIGN KEY (id_pacote) REFERENCES Pacotes(id_pacote)
 );
-@ CREATE TABLE Pagamentos (
+ CREATE TABLE Pagamentos (
 id_pagamento INT PRIMARY KEY AUTO_INCREMENT,
 id_reserva INT,
 valor DECIMAL(10, 2),
