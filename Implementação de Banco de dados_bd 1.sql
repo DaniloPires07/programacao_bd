3e218b79cca6dcb925e69c565a3215876a264a58
DaniloPires07 <danilodanilopires1@gmail.com>
2024-09-09 14:17:19 -0300

    Create Implementação de Banco de dados_bd 1.sql

---
+++ Implementação de Banco de dados_bd 1.sql
@@ -0,0 +1,8 @@
+/*
+SENAC DF - CEP Jessé Freire
+2024.07.276 - Aprendizagem Profissional de Qualificação em Desenvolvimento de Softwares
+Prof® Hudson Neves
+Projeto Final - Implementação de Banco de Dados Relacional com MySQ]
+Projeto - Clinica Odontológica OdontoVida */
+- 1. Criação do Arquivo/Banco de Dados create database OdontoVida;
+-- 2. Acessando Arquivo/Banco de Dados use OdontoVida;
